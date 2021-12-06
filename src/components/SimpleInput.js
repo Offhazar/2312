ec5e26a6274096b0d494b76ddada857a73a1a843
Almazbek uulu Adilet <aduletalmaz@gmail.com>
2021-12-06 17:12:45 +0300

    'f'

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,30 +3,50 @@ import { useState } from 'react';
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enterenNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredTouched, setEnteredTouched] = useState(false);
   const nameInputChangeNudler = (e) => {
     setEnteredName(e.target.value);
   };
 
-  const formSumbitNudler = (e) => {
-    e.preventDefault();
+  const nameInputBlurHandler = (e) => {
+    setEnteredTouched(true);
 
     if (enteredName.trim() === '') {
       setEnteredNameIsValid(false);
       return;
     }
+  };
+
+  const formSumbitNudler = (e) => {
+    e.preventDefault();
+
+    setEnteredTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
   };
 
-  const nameInputClasses = enterenNameIsValid
-    ? 'form-control'
-    : 'form-control error-text';
+  const nameInputIsInvalid = !enterenNameIsValid && enteredTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control error-text'
+    : 'form-control';
   return (
     <form onSubmit={formSumbitNudler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeNudler} />
-        {!enterenNameIsValid && <p>Name must not be empty</p>}
+        <input
+          onBlur={nameInputBlurHandler}
+          type="text"
+          id="name"
+          onChange={nameInputChangeNudler}
+        />
+        {nameInputIsInvalid && <p>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
